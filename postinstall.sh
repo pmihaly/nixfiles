b38ac6d4a871e01859d167a2644edbed2677783b
pmihaly <mishreg@protonmail.com>
2021-03-27 15:47:16 +0100

    Spacemacs install and doom emacs config

--- postinstall.sh
+++ postinstall.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Doom emacs
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+helpmessage() { \
+    echo "USAGE: "
+    echo "$0 emacs (for Doom Emacs)"
+    echo "$0 vim (for SpaceVim)" 
+    exit 0
+}
 
-cat > ~/.local/share/applications/doom-emacs.desktop <<EOF
+installdoomemacs() { \
+    
+    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+    ~/.emacs.d/bin/doom install
+    
+    # Create desktop entry with fix for pantheon DE
+    cat > ~/.local/share/applications/doom-emacs.desktop << EOF
 [Desktop Entry]
 
 # The type as listed above
@@ -30,20 +39,61 @@ Icon=emacs
 Terminal=false
 EOF
 
+    # Relative line numbers
+    sed -i "s/type t/type \"relative\"/g" ~/.doom.d/config.el
+    
+    # Set org-directory to ~/sync/org
+    sed -i "s/~\/org\//~\/sync\/org\//g" ~/.doom.d/config.el
+    
+    # Automatic fullscreen
+    cat >> ~/.doom.d/config.el << EOF
+(add-to-list 'default-frame-alist '(fullscreen . fullscreen))
+EOF
+    # Some packages in init.el
+    sed -i "s/doom-quit/;;doom-quit/g" ~/.doom.d/init.el
+    sed -i "s/;;(emoji/(emoji/g" ~/.doom.d/init.el
+    sed -i "s/;;ligatures/ligatures/g" ~/.doom.d/init.el
+    sed -i "s/;;treemacs/treemacs/g" ~/.doom.d/init.el
+    sed -i "s/;;zen/zen/g" ~/.doom.d/init.el
+    sed -i "s/;;(format/(format/g" ~/.doom.d/init.el
+    sed -i "s/;;vterm/vterm/g" ~/.doom.d/init.el
+    sed -i "s/;;shell/shell/g" ~/.doom.d/init.el
+    sed -i "s/;;lsp/lsp/g" ~/.doom.d/init.el
+    sed -i "s/;;pdf/pdf/g" ~/.doom.d/init.el
+    sed -i "s/;;make/make/g" ~/.doom.d/init.el
+    sed -i "s/;;data/data/g" ~/.doom.d/init.el
+    sed -i "s/;;nix/nix/g" ~/.doom.d/init.el
+    sed -i "s/;;web/web/g" ~/.doom.d/init.el
+    sed -i "s/;;yaml/yaml/g" ~/.doom.d/init.el
+
+    ~/.emacs.d/bin/doom sync
+}
+
+installspacevim() { \
+   curl -sLf https://spacevim.org/install.sh | bash 
+}
+
+[ $# -eq 0 ] && helpmessage
+
+case "$1" in
+    emacs)  installdoomemacs ;;
+    vim)    installspacevim ;;    
+    *)      helpmessage ;;
+esac
+
 # Remap caps lock to escape
-# TODO kitalálni hogy hogyan futtassuk le ezeket amikor a felhasználó belép a GDM-be (.xsession hibát ír ki, .xprofile nem fut le)
-cat > ~/.xprofile <<EOF
+cat > ~/.xprofile << EOF
 setxkbmap -option "caps:escape"
 xset r rate 300 50
 EOF
 
 # Luke Smith's ZSH config (from https://github.com/LukeSmithxyz/voidrice)
-cat > .zshrc <<EOF
+cat > ~/.zshrc << EOF
 # Luke's config for the Zoomer Shell
 
 # Enable colors and change prompt:
 autoload -U colors && colors	# Load colors
-PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
+PS1="%B%{\$fg[red]%}[%{\$fg[yellow]%}%n%{\$fg[green]%}@%{\$fg[blue]%}%M %{\$fg[magenta]%}%~%{\$fg[red]%}]%{\$reset_color%}$%b "
 setopt autocd		# Automatically cd into typed directory.
 stty stop undef		# Disable ctrl-s to freeze terminal.
 setopt interactive_comments
@@ -79,22 +129,25 @@ bindkey -v '^?' backward-delete-char
 # Use lf to switch directories and bind it to ctrl-o
 lfcd () {
     tmp="$(mktemp)"
-    lf -last-dir-path="$tmp" "$@"
-    if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
-        rm -f "$tmp" >/dev/null
-        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+    lf -last-dir-path="\$tmp" "$@"
+    if [ -f "\$tmp" ]; then
+        dir="\$(cat "\$tmp")"
+        rm -f "\$tmp" >/dev/null
+        [ -d "\$dir" ] && [ "\$dir" != "$(pwd)" ] && cd "\$dir"
     fi
 }
 bindkey -s '^o' 'lfcd\n'
 
 bindkey -s '^a' 'bc -lq\n'
 
-bindkey -s '^f' 'cd "$(dirname "$(fzf)")"\n'
+bindkey -s '^f' 'cd "\$(dirname "\$(fzf)")"\n'
 
 bindkey '^[[P' delete-char
 
 # Edit line in vim with ctrl-e:
 autoload edit-command-line; zle -N edit-command-line
 bindkey '^e' edit-command-line
+
 EOF
+
+echo "Done configuring nearly everything, next step is to install needed plugins in your editor."
